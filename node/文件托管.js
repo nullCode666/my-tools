70016a6a24c29f7691c9323b713eb57426e6d733
kevinzhou <2269809137@qq.com>
2024-06-11 16:52:00 +0800

    111

--- node/文件托管.js
+++ node/文件托管.js
@@ -2,9 +2,9 @@ const express = require("express");
 
 const app = express();
 
-const port = 3000;
+const port = 3001;
 
-app.use(express.static('public'));
+app.use("/",express.static('public/official-website-20240611'));
 
 // 开始监听指定端口
 app.listen(port, () => {
